d8781527407d8235d588f2e15656a2a31df5ffdd
hyerimJU <110883894+hyerimJU@users.noreply.github.com>
2022-12-08 17:53:57 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,54 @@
+// 
+//팝업 띄우기
+function openpop() {
+    document.getElementById("popup_layer1").style.display = "block";
+}
+function openpop2() {
+    document.getElementById("popup_layer2").style.display = "block";
+}
+function openpop3() {
+    document.getElementById("popup_layer3").style.display = "block";
+}
+function openpop4() {
+    document.getElementById("popup_layer4").style.display = "block";
+}
+function openpop5() {
+    document.getElementById("popup_layer5").style.display = "block";
+}
+//팝업 닫기
+function closePop() {
+    document.getElementById("popup_layer1").style.display = "none";
+}
+function closePop2() {
+    document.getElementById("popup_layer2").style.display = "none";
+}
+function closePop3() {
+    document.getElementById("popup_layer3").style.display = "none";
+}
+function closePop4() {
+    document.getElementById("popup_layer4").style.display = "none";
+}
+function closePop5() {
+    document.getElementById("popup_layer5").style.display = "none";
+}
+
+function openpop(modalname) {
+    document.get
+    $("." + modalname).fadeIn(300);
+}
+function openpop2(modalname) {
+    document.get
+    $("." + modalname).fadeIn(300);
+}
+function openpop3(modalname) {
+    document.get
+    $("." + modalname).fadeIn(300);
+}
+function openpop4(modalname) {
+    document.get
+    $("." + modalname).fadeIn(300);
+}
+function openpop5(modalname) {
+    document.get
+    $("." + modalname).fadeIn(300);
+}
---
+++ js/script.js
@@ -0,0 +1,13 @@
+// typing animation
+var typed = new Typed(".typing1", {
+    strings: [
+        "만나서 반갑습니다.",
+        "Nice to meet you.",
+        "お会あいできてうれしいです。",
+        "见到您很高兴。",
+        "hân hạnh được gặp bạn"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
